9e1a7476c1d6684d8f929aa20c9dcabe8b1f8b32
Nishanth2244 <thotanishanthnetha222@gmail.com>
2026-05-28 17:15:57 +0530

    schedular Enablescheduing

--- src/main/java/com/Project/Mechanic/MechanicApplication.java
+++ src/main/java/com/Project/Mechanic/MechanicApplication.java
@@ -3,8 +3,11 @@ package com.Project.Mechanic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
+
+@EnableScheduling
 @SpringBootApplication
 public class MechanicApplication {
 
