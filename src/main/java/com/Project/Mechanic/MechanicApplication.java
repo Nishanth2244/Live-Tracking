194e335bbe94b77dc625d46589336d7dfbacd63b
Nishanth2244 <thotanishanthnetha222@gmail.com>
2026-05-28 16:56:59 +0530

    schedular

--- src/main/java/com/Project/Mechanic/MechanicApplication.java
+++ src/main/java/com/Project/Mechanic/MechanicApplication.java
@@ -2,6 +2,8 @@ package com.Project.Mechanic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MechanicApplication {
@@ -9,4 +11,9 @@ public class MechanicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MechanicApplication.class, args);
 	}
+	
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
